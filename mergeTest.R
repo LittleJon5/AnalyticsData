9fe27ee084531853ea21a5d351097c315ab21ffc
LittleJon5 <littleriz@gmail.com>
2015-07-29 15:27:06 -0600

    Commit message

--- mergeTest.R
+++ mergeTest.R
@@ -2,9 +2,5 @@ ggplot(data = mtcars, aes(x = wt, y = mpg)) +
   geom_point() +
   geom_hline(yintercept = 25) +
   geom_line() +
-<<<<<<< HEAD
-  geom_smooth(method = "lm", span = 0.2, color = "red") +
-=======
   geom_smooth(method = "lm", span = 0.2, color = "green") +
->>>>>>> 544e26a59681344b62c0e38a5bae4451d282223d
   facet_wrap( ~ cyl)
