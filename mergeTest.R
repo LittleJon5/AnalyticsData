d94419017bb21d62aa94a8f76430c72e541a6763
Ray Nelson <raydnelson@live.com>
2015-07-29 15:05:33 -0600

    Comment out geom_line

--- mergeTest.R
+++ mergeTest.R
@@ -1,5 +1,5 @@
 ggplot(data = mtcars, aes(x = wt, y = mpg)) +
   geom_point() +
-  geom_line() +
+  # geom_line() +
   geom_smooth(method = "lm", span = 0.2, color = "yellow") +
   facet_wrap( ~ cyl)
